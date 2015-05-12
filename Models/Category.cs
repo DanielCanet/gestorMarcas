328329960825d001877a1ea77791e7ad3ee9fffe
DaniDog <minombreesdani@yahoo.com>
2015-05-12 09:41:13 +0200

    Refactor para eliminar ICategory e MasterCategory substituido por la clase Category

---
+++ Models/Category.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gestorMarcas.Models
+{
+    public class Category
+    {
+        private Athlete athlete { get; set; }
+
+        public Category(Athlete athlete)
+        {
+            this.athlete = new Athlete();
+            this.athlete = athlete;
+        }
+
+        private int GetYearsOld()
+        {
+            int diffYear = DateTime.Today.Year - this.athlete.BornDate.Year;
+
+            if (DateTime.Today.Month < this.athlete.BornDate.Month)
+            {
+                return diffYear - 1;
+            }
+            else if (DateTime.Today.Month == this.athlete.BornDate.Month)
+            {
+                if (DateTime.Today.Day < this.athlete.BornDate.Day) { return diffYear - 1; }
+                else if (DateTime.Today.Day == this.athlete.BornDate.Day) { return diffYear; }
+                else { return diffYear; }
+            }
+            else
+            {
+                return diffYear;
+            }
+        }
+
+        public string GetCategory()
+        {
+            int result = this.GetYearsOld() / 5 * 5;
+            return this.athlete.Sex + result.ToString();
+        }
+
+        public string GetYearCategory()
+        {
+            return this.athlete.Sex + this.GetYearsOld().ToString();
+        }
+    }
+}
